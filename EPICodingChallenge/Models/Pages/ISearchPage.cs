6e8c88afd0192b448c910cc76edf5aa728258c09
fchowdhury <fchowdhury@atb.com>
2020-05-05 23:07:27 -0600

    Add project files.

---
+++ EPICodingChallenge/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace EPICodingChallenge.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+User Acounts:
+	1. Admin : Username: Admin, pass: Admin-123789

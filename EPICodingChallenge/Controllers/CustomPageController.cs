46b32ccda05225fc91d8eb777cdd463919c3dc3d
fchowdhury <fchowdhury@atb.com>
2020-05-07 02:50:08 -0600

    Added skeleton controller and view

---
+++ EPICodingChallenge/Controllers/CustomPageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPICodingChallenge.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace EPICodingChallenge.Controllers
+{
+    public class CustomPageController : PageControllerBase<CustomPage>
+    {
+        public ActionResult Index(CustomPage currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(PageViewModel.Create(currentPage));
+        }
+    }
+}
